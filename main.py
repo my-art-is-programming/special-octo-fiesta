999f769a150a64cdf87d699877fd6100ba719664
artem <artem.dermnezhi@gmail.com>
2022-04-26 22:12:25 +0300

    Add function for generating random numbers

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import random
 
+
+def randgen(x):
+    """
+    generating amount number of random numbers
+    :param x: quantity of random numbers
+    :return:
+    """
+    i = 0
+    while i < x:
+        i += 1
+        yield random.randint(1, 100)
+
+
 if __name__ == '__main__':
-    print(random.randint(1, 100))
+    for j in randgen(5):
+        print(j)
+
+    print([x for x in randgen(5)])
