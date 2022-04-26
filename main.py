95885260f5e145d2f39f2ccd68b0a179989d0caa
artem <artem.dermnezhi@gmail.com>
2022-04-26 23:33:04 +0300

    Implemented argparse

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import argparse
+
+
 arr = [0, 12, 29, 52, 60, 63, 70, 93, 99, 123, 130, 134, 139, 139, 182, 195, 212, 218, 222, 224, 228, 234, 235, 237,
        244, 255, 259, 269, 271, 288, 297, 297, 299, 310, 316, 332, 353, 377, 378, 387, 391, 418, 431, 442, 444, 459,
        460, 464, 490, 571, 599, 605, 634, 637, 647, 654, 656, 663, 669, 683, 689, 691, 710, 713, 715, 720, 722, 732,
@@ -20,7 +23,12 @@ def binarysearch(n: int, x: list):
             low = mid + 1
 
 
+parser = argparse.ArgumentParser(description='Process some number.')
+parser.add_argument('integers', type=int, help='number to find in array')
+args = parser.parse_args()
+print(args.integers)
+
+
 if __name__ == '__main__':
-    a = binarysearch(0, arr)
-    print(arr[a])
+    a = binarysearch(args.integers, arr)
     print(a)
